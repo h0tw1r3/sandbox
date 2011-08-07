6014651fb4f171d3b8d08e136089db215583e38b
Jeffrey Clark <dude@zaplabs.com>
2011-08-07 14:34:02 +0000

    require uri match and exclude seperator from capture

--- qa/stackexchange/serverfault/295664/api.php
+++ qa/stackexchange/serverfault/295664/api.php
@@ -1,21 +1,22 @@
 <?php
 
+header('Content-type: text/plain');
+
 // Define the REQUEST_URI to remove from the request
 // and how to split what is interesting.
 define('STRIP_URI', '/^.+\/calc\//U');
-define('REGEX_URI', '/^(?P<calc>.+)(?P<ext>\.(txt|sci))?$/U');
+define('REGEX_URI', '/^(?P<calc>.+)(\.(?P<ext>[\w]+))?$/U');
 
 // Extend to prevent injection attacks
-$clean_request_uri = rawurldecode(preg_replace(STRIP_URI, '', $_SERVER['REQUEST_URI']));
+$found_uri = 0;
+$clean_request_uri = rawurldecode(preg_replace(STRIP_URI, '', $_SERVER['REQUEST_URI'], -1, $found_uri));
 
 $math = array();
 preg_match(REGEX_URI, $clean_request_uri, $math);
 
 // Perform checks to make sure math is valid?
-if (!isset($math['calc'])) {
+if (!$found_uri || !isset($math['calc'])) {
     die('Invalid calculation');
 }
 
-echo $math['calc'] . "<br/>";
-echo $math['ext'] . "<br/>";
-
+print_r($math);
